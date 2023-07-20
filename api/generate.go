12e4f7d8a2facf6d1a4dfeba4ae53f3a6ef60400
Denis Mishin <dmishin@pomerium.com>
2023-07-20 17:23:09 -0400

    scaffolding

---
+++ api/generate.go
@@ -0,0 +1,5 @@
+package openapi
+
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=models.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=client.yaml openapi.yaml
