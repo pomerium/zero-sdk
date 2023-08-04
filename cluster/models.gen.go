2543fc83278d54581f9ae96220504d7aed095923
Denis Mishin <dmishin@pomerium.com>
2023-08-04 10:22:24 -0400

    api: bootstrap config (#11)

--- cluster/models.gen.go
+++ cluster/models.gen.go
@@ -7,6 +7,12 @@ const (
 	BearerAuthScopes = "bearerAuth.Scopes"
 )
 
+// BootstrapConfig defines model for BootstrapConfig.
+type BootstrapConfig struct {
+	// DatabrokerStorageConnection databroker storage connection string
+	DatabrokerStorageConnection *string `json:"databrokerStorageConnection,omitempty"`
+}
+
 // Bundle defines model for Bundle.
 type Bundle struct {
 	// Id bundle id
@@ -40,6 +46,9 @@ type ExchangeTokenResponse struct {
 	// IdToken ID token
 	IdToken string `json:"idToken"`
 }
+
+// GetBootstrapConfigResponse defines model for GetBootstrapConfigResponse.
+type GetBootstrapConfigResponse = BootstrapConfig
 
 // GetBundlesResponse defines model for GetBundlesResponse.
 type GetBundlesResponse struct {
