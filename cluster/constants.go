d7a0470bc122046e06cee3fc6cb4078a9bb17c7b
Denis Mishin <dmishin@pomerium.com>
2023-10-24 15:46:51 -0400

    add bundle constants

---
+++ cluster/constants.go
@@ -0,0 +1,8 @@
+package cluster
+
+const (
+	// ClusterBootstrapConfigBundleName is the name of the bootstrap config bundle
+	ClusterBootstrapConfigBundleName = "bootstrap-config"
+	// ClusterConfigBundleName is the name of the main config bundle
+	ClusterConfigBundleName = "config"
+)
